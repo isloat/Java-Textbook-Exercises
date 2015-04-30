35bda921f301894ff2793813ae3066bcb2f2dc68
Isaac Sloat <isaac.sloat@gmail.com>
2015-04-30 18:40:57 -0500

    6.4b

--- Chapter 6/src/SumWithoutSmallest.java
+++ Chapter 6/src/SumWithoutSmallest.java
@@ -4,17 +4,27 @@ import java.util.Arrays;
 public class SumWithoutSmallest {
 	public static void main(String[] args) {
 		Random random = new Random();
-		int[] array = new int[10];
+		double[] array = new double[10];
 		
 		for (int i = 0; i < array.length; i++) { // initialize every position in the array with a random value
 			array[i] = random.nextInt(1000);
 		}
+		
+		System.out.println(Arrays.toString(array));
+		System.out.println(sumWithoutSmallest(array));
 	}
 	
 	public static double sumWithoutSmallest(double[] array) {
 		double sum = 0;
-		double smallest = 0;
+		double smallest = array[0];
 		
-		return sum;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+			if (array[i] <= smallest) {
+				smallest = array[i];
+			}
+		}
+		
+		return (sum - smallest);
 	}
 }
