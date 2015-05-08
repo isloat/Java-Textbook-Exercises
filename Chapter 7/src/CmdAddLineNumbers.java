89c49073a4899647e92d3efa1a2ef537a8047ec5
Isaac Sloat <isaac.sloat@gmail.com>
2015-05-07 22:03:29 -0500

    7.3
    Update 7.2 to take command line arguments if applicable.

---
+++ Chapter 7/src/CmdAddLineNumbers.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class CmdAddLineNumbers {
+	static int lineNumber = 1;
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner input = new Scanner(System.in);
+		String filenameRead;
+		String filenameWrite;
+		
+		if (args.length == 2) {
+			filenameRead = args[0];
+			filenameWrite = args[1];
+		} else {
+			System.out.print("Input filename to read: ");
+			filenameRead = input.next();
+			System.out.print("Input filename to write to: ");
+			filenameWrite = input.next();
+		}
+		
+		input.close();
+		
+		File fileRead = new File(filenameRead);
+		Scanner fileIn = new Scanner(fileRead);
+		PrintWriter fileOut = new PrintWriter(filenameWrite);
+		
+		while (fileIn.hasNextLine()) {
+			String line = fileIn.nextLine();
+			System.out.println(line);
+			writeLine(line, fileOut);
+		}
+		
+		fileIn.close();
+		fileOut.close();
+	}
+	
+	public static void writeLine(String line, PrintWriter fileOut) {
+		fileOut.println(String.format("/* %d */ %s", lineNumber, line));
+		lineNumber += 1;
+	}
+}
