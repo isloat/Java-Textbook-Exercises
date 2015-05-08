847ee78d9307bcbb46516f2c9d4cbccbcf967b0a
Isaac Sloat <isaac.sloat@gmail.com>
2015-05-07 22:00:19 -0500

    7.2
    After working through this chapter, I am tempted to go back over all my
    earlier exercises and close my Scanner objects.

---
+++ Chapter 7/src/AddLineNumbers.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class AddLineNumbers {
+	static int lineNumber = 1;
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input filename to read: ");
+		String filenameRead = input.next();
+		System.out.print("Input filename to write to: ");
+		String filenameWrite = input.next();
+		input.close();
+		
+		File fileRead = new File(filenameRead);
+		Scanner fileIn = new Scanner(fileRead);
+		PrintWriter fileOut = new PrintWriter(filenameWrite);
+		
+		while (fileIn.hasNextLine()) {
+			String line = fileIn.nextLine();
+			System.out.println(line);
+			writeLine(line, fileOut);
+		}
+		
+		fileIn.close();
+		fileOut.close();
+	}
+	
+	public static void writeLine(String line, PrintWriter fileOut) {
+		fileOut.println(String.format("/* %d */ %s", lineNumber, line));
+		lineNumber += 1;
+	}
+}
