d4338482039ad57c54ab8d5cd3c274e830752e0f
Isaac Sloat <isaac.sloat@gmail.com>
2015-05-11 14:04:30 -0500

    8.2

---
+++ Chapter 8/src/TallyCounter.java
@@ -0,0 +1,31 @@
+/* This will simulate a tally counter that admits a limited number of people.
+ * Set the limit with a call to setLimit(int maximum).
+ * If count surpasses the limit, alert by printing "Limit exceeded."
+ * 
+ * This modifies Exercise 8.2.
+ */
+
+public class TallyCounter {
+	private int value;
+	private int maximum;
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setLimit(int maximum) {
+		this.maximum = maximum;
+	}
+	
+	public void count() {
+		if (value + 1 > maximum) {
+			System.out.println("Limit exceeded.");
+		} else value++;
+	}
+	
+	public void undo() {
+		if (value > 0) {
+			value --;
+		}
+	}
+}
