103b2527c49f4ae0f2889c4387056448e16ec760
Isaac Sloat <isaac.sloat@gmail.com>
2015-05-11 13:47:10 -0500

    8.1

---
+++ Chapter 8/src/UndoButton.java
@@ -0,0 +1,31 @@
+/* This program will add a method, undo(), that adds
+ * a button that allows an operator to
+ * undo an accidental button click. 
+ * 
+ * Make sure that the operator cannot click this button more
+ * often than they can click the count button.
+ * 
+ * Extension of Exercise 8.2 from the book.
+ */
+
+public class UndoButton {
+	private int value;
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void count() {
+		value++;
+	}	
+	
+	public void undo() {
+		if (value > 0) {
+			value --;
+		}
+	}
+	
+	public void reset() {
+		value = 0;
+	}
+}
