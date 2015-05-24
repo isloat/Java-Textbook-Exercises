f683c266c7aa1242ed94c39f34150b9d22ab0437
Isaac Sloat <isaac.sloat@gmail.com>
2015-05-23 19:20:42 -0500

    9.13

--- Chapter 9/src/LabeledPoint.java
+++ Chapter 9/src/LabeledPoint.java
@@ -1,16 +1,14 @@
+import java.awt.Point;
 
-public class LabeledPoint {
-	int x;
-	int y;
-	String label;
+public class LabeledPoint extends Point {
+	private String label;
 	
 	public LabeledPoint(int x, int y, String label) {
-		this.x = x;
-		this.y = y;
+		super(x, y);
 		this.label = label;
 	}
 	
 	public String toString() {
-		return String.format("%s [x = %d, y = %d, label = %s]", getClass().getName(), this.x, this.y, this.label);
+		return super.toString() + ", label = " + this.label;
 	}
 }
