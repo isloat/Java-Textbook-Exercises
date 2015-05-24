4d9083a67f6337d3e37fdef8b0c6f216f789be01
Isaac Sloat <isaac.sloat@gmail.com>
2015-05-23 19:06:01 -0500

    9.10

---
+++ Chapter 9/src/BetterRectangle.java
@@ -0,0 +1,16 @@
+import java.awt.Rectangle;
+
+public class BetterRectangle extends Rectangle {
+	public BetterRectangle(int x, int y, int width, int height) {
+		super.setLocation(x, y);
+		super.setSize(width, height);
+	}
+	
+	public double getPerimeter() {
+		return (super.getWidth() * 2 + super.getHeight() * 2);
+	}
+	
+	public double getArea() {
+		return (super.getWidth() * super.getHeight());
+	}
+}
