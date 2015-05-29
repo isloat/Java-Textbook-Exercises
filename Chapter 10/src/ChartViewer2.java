737835e5688747a3b34bc2b6a143bf6f07543e16
Isaac Sloat <isaac.sloat@gmail.com>
2015-05-29 18:54:40 -0500

    ChartViewer2

---
+++ Chapter 10/src/ChartViewer2.java
@@ -0,0 +1,17 @@
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class ChartViewer2 {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		
+		frame.setSize(400, 200);
+		frame.setTitle("Another bar chart");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JComponent component = new ChartComponent2();
+		frame.add(component);
+		
+		frame.setVisible(true);
+	}
+}
