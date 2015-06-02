49c28a1b3681097eb07fe73bb1376f8352120eba
Isaac Sloat <isaac.sloat@gmail.com>
2015-06-01 22:33:21 -0500

    10.3
    Had to rework large parts of this...

--- Chapter 10/src/ButtonFrame2.java
+++ Chapter 10/src/ButtonFrame2.java
@@ -1,11 +1,12 @@
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class ButtonFrame2 extends JFrame {
-	private JButton button;
-	private JLabel label;
-	private int n;
 	
 	private static final int FRAME_WIDTH = 300;
 	private static final int FRAME_HEIGHT = 300;
@@ -16,28 +17,26 @@ public class ButtonFrame2 extends JFrame {
 	}
 	
 	private void createComponents() {
-		button = new JButton("Click me.");
-		ActionListener listener = new ClickListener();
-		button.addActionListener(listener);
+		JButton button = new JButton("Click me.");
+		JButton button2 = new JButton("Click me too.");
 		
-		label = new JLabel("Hello, world.");
+		ActionListener listener = new ClickListener();
+		ActionListener listener2 = new ClickListener();
+		button.addActionListener(listener);
+		button2.addActionListener(listener2);
 		
 		JPanel panel = new JPanel();
 		panel.add(button);
-		panel.add(label);
+		panel.add(button2);
 		add(panel);
 	}
 	
 	class ClickListener implements ActionListener {
+		private int n;
+		
 		public void actionPerformed(ActionEvent event) {
 			n++;
-			label.setText("I was clicked " + n + " times.");
+			System.out.println("I was clicked " + n + " time" + (n >= 2? "s." : "."));
 		}
-	}
-	
-	public static void main(String[] args) {
-		JFrame frame = new ButtonFrame1();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
 	}
 }
