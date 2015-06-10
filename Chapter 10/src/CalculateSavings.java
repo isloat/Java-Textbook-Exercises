03ba8dc3c66ef29c566676ebd7f7567a9a03a2a1
Isaac Sloat <isaac.sloat@gmail.com>
2015-06-09 23:42:19 -0500

    10.12a
    More internal guts.

--- Chapter 10/src/CalculateSavings.java
+++ Chapter 10/src/CalculateSavings.java
@@ -37,10 +37,42 @@ public class CalculateSavings extends JFrame {
     
     // Create labels and initialize button creation.
     private void createComponents() {
-    	
+        final int INPUT_FIELD_SIZE = 10;
+        
+        this.inputInitialBalanceLabel = new JLabel("Initial balance: ");
+        this.inputInitialBalanceLabelField = new JTextField(INPUT_FIELD_SIZE);
+        this.inputAnnualInterestRate = new JLabel("Annual interest rate: ");
+        this.inputAnnualInterestRateField = new JTextField(INPUT_FIELD_SIZE);
+        this.inputNumberYears = new JLabel("Number of years: ");
+        this.inputNumberYearsField = new JTextField(INPUT_FIELD_SIZE);
+        
+        this.createButton();
     }
     
+    // Create button and action listeners.
+    private void createButton() {
+        this.calculateButton = new JButton("Calculate");
+        this.calculateButton.addActionListener(new ActionListener() {
+        	
+            public void actionPerformed(ActionEvent e) {
+                try {
+                } catch (Exception exception) {
+                }
+            }
+        });
+    }
+    
+    // Set and enable everything.
     private void createPanel() {
-    	
+        JPanel panel = new JPanel();
+        
+        panel.add(this.inputInitialBalanceLabel);
+        panel.add(this.inputInitialBalanceLabelField);
+        panel.add(this.inputAnnualInterestRate);
+        panel.add(this.inputAnnualInterestRateField);
+        panel.add(this.inputNumberYears);
+        panel.add(this.inputNumberYearsField);
+        
+        panel.add(this.calculateButton);
     }
 }
