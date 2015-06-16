78ec155f4c30f9f79b02a58f05900759f123f169
Isaac Sloat <isaac.sloat@gmail.com>
2015-06-15 22:59:44 -0500

    10.14

--- Chapter 10/src/NameComponent.java
+++ Chapter 10/src/NameComponent.java
@@ -1,6 +1,6 @@
 import java.util.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.*;
 
@@ -9,9 +9,34 @@ import javax.swing.*;
  * contained inside a blue rectangle. Provide a class NameViewer
  * and a class NameComponent.
  */
-public class NameComponent {
+public class NameComponent extends JComponent {
+//	private static final int FRAME_WIDTH = 300;
+//	private static final int FRAME_HEIGHT = 100;
+//	
+//	public NameComponent() {
+//	    this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+//	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//	    this.setVisible(true);
+//	    
+//	    this.createComponents();
+//	}
 
-	public NameComponent() {
+	public void createComponents(Graphics graphics) {
+		Graphics2D graphics2 = (Graphics2D) graphics;
 		
+		int width = this.getWidth() - 10;
+		int height = this.getHeight() - 10;
+		int x = 5;
+		int y = 5;
+		graphics2.setColor(Color.blue);
+		graphics2.fillRect(x, y, width, height);
+		
+		String name = "Isaac Sloat";
+		Rectangle2D stringBounds = graphics2.getFontMetrics().getStringBounds(name, graphics2);
+		
+		x = (int) (x + width / 2 - stringBounds.getCenterX());
+	    y = (int) (y + height / 2 - stringBounds.getCenterY());
+	    graphics2.setColor(Color.red);
+	    graphics2.drawString(name, x, y);
 	}
 }
