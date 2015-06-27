67c8179d16123b9323250bbc5e7f1d7ad7c56c5c
Isaac Sloat <isaac.sloat@gmail.com>
2015-06-27 18:39:17 -0500

    10.16

---
+++ Chapter 10/src/TwoSquareComponent.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.*;
+
+/*
+ * 10.16: Write a  program that draws two solid squares: one in pink and one in purple.
+ * Use a standard color for one of them and a custom color for the second. Provide a
+ * class TwoSquareViewer and a class TwoSquareComponent.
+ */
+public class TwoSquareComponent extends JComponent {
+	public void paint(Graphics graphics) {
+		Graphics2D graphics2 = (Graphics2D) graphics;
+	}
+}
